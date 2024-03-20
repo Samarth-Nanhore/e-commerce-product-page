333270d1336c6a1f46ef40a2e353fae53b6bae97
samarth01 <84143644+Samarth-Nanhore@users.noreply.github.com>
2024-03-20 18:46:18 +0530

    Create variable to store DOM element  and apply click event listener on 'quantityIncrementBtn' in script.js
    DOM elements
    - img.plus-btn
    - p.quantity-value

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
+const quantityIncrementBtn = document.querySelector("img.plus-btn");
+const quantityValueElement = document.querySelector("p.quantity-value");
 
+quantityIncrementBtn.addEventListener("click");
