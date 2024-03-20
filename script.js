6f15d9f1f2e759a0bfb1b81e7b6acdc9d533e9e5
samarth01 <84143644+Samarth-Nanhore@users.noreply.github.com>
2024-03-20 19:01:46 +0530

    Write function incrementProductQuantity() for click event of DOM element variable quantityIncrementBtn
    - write function convertTextContentToNumber to convert string to number eg. "1" to 1

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const quantityIncrementBtn = document.querySelector("img.plus-btn");
 const quantityValueElement = document.querySelector("p.quantity-value");
 
-quantityIncrementBtn.addEventListener("click");
+quantityIncrementBtn.addEventListener("click", incrementProductQuantity);
+
+function incrementProductQuantity() {
+  let quantityValue = convertTextContentToNumber(
+    quantityValueElement.textContent
+  );
+
+  if (quantityValue > 0 && quantityValue < 5) {
+    let addOne = quantityValue + 1;
+    quantityValueElement.textContent = `${addOne}`;
+  }
+}
+
+function convertTextContentToNumber(string) {
+  // string must be value that can convert into number
+  return Number(string);
+}
